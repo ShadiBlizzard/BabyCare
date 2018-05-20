e26568bf50a9f0e97b1de44a18d25e5304d0bf72
Giulia <giulia.pennati94@gmail.com>
2018-05-20 21:39:02 +0200

    starting style parent home

--- app/src/main/java/com/example/shadi/babycare/layout_view/LoginActivity.java
+++ app/src/main/java/com/example/shadi/babycare/layout_view/LoginActivity.java
@@ -4,15 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toolbar;
 
 import com.example.shadi.babycare.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getActionBar().setTitle("Login");
     }
 
     public void login(View view) {
