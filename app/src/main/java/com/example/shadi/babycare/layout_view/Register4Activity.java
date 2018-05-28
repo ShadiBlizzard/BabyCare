1badff3a8080a42bf4fcf757454fe030041fafcb
Giulia <giulia.pennati94@gmail.com>
2018-05-28 20:46:45 +0200

    layout page 4 registration, IMG uploading management

--- app/src/main/java/com/example/shadi/babycare/layout_view/Register4Activity.java
+++ app/src/main/java/com/example/shadi/babycare/layout_view/Register4Activity.java
@@ -1,14 +1,26 @@
 package com.example.shadi.babycare.layout_view;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.shadi.babycare.R;
 
 public class Register4Activity extends AppCompatActivity {
 
     private Bundle bundle;
+    private static final int SELECT_PICTURE = 0;
+    private ImageView img;
+    private Button upload;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +28,49 @@ public class Register4Activity extends AppCompatActivity {
         Intent intent = getIntent();
         bundle = intent.getExtras();
 
+        img = findViewById(R.id.imageView);
+        upload = findViewById(R.id.upload);
 
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Bitmap bitmap = getPath(data.getData());
+            img.setImageBitmap(bitmap);
+        }
+    }
+
+
+    private Bitmap getPath(Uri uri) {
+
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String filePath = cursor.getString(column_index);
+        // cursor.close();
+        // Convert file path into bitmap image using below line.
+        Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+
+        return bitmap;
+    }
+
+
+    private void selectImage() {
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
     }
 }
