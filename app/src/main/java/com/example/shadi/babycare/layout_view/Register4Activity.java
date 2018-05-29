2bc9ba7f54c5c6d2bec14fa9dbc05dd8574dba9b
Giulia <giulia.pennati94@gmail.com>
2018-05-29 21:21:34 +0200

    ended registration phase pages

--- app/src/main/java/com/example/shadi/babycare/layout_view/Register4Activity.java
+++ app/src/main/java/com/example/shadi/babycare/layout_view/Register4Activity.java
@@ -1,23 +1,45 @@
 package com.example.shadi.babycare.layout_view;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
 import android.net.Uri;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.shadi.babycare.R;
 
-public class Register4Activity extends AppCompatActivity {
+import net.gotev.uploadservice.MultipartUploadRequest;
+import net.gotev.uploadservice.UploadNotificationConfig;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpParams;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Register4Activity extends AppCompatActivity implements View.OnClickListener{
 
     private Bundle bundle;
-    private static final int SELECT_PICTURE = 0;
+    private static final int SELECTED_PICTURE = 0;
     private ImageView img;
     private Button upload;
 
@@ -31,46 +53,64 @@ public class Register4Activity extends AppCompatActivity {
         img = findViewById(R.id.imageView);
         upload = findViewById(R.id.upload);
 
-        img.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                selectImage();
-            }
-        });
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            Bitmap bitmap = getPath(data.getData());
-            img.setImageBitmap(bitmap);
+        if (requestCode == SELECTED_PICTURE && resultCode == RESULT_OK && data!= null) {
+            Uri selectedImage = data.getData();
+            img.setImageURI(selectedImage);
         }
     }
 
-
-    private Bitmap getPath(Uri uri) {
-
-        String[] projection = {MediaStore.Images.Media.DATA};
-        Cursor cursor = managedQuery(uri, projection, null, null, null);
-        int column_index = cursor
-                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-        cursor.moveToFirst();
-        String filePath = cursor.getString(column_index);
-        // cursor.close();
-        // Convert file path into bitmap image using below line.
-        Bitmap bitmap = BitmapFactory.decodeFile(filePath);
-
-        return bitmap;
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageView:
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, SELECTED_PICTURE);
+                break;
+            case R.id.upload:
+                //TODO how to send to the server the image uploaded
+                /*basing on the result of the registration, we could have two results, one of
+                correctness that call RegisterOkActivity, One with error that will call RegiterErrorActivity
+                NOW I HARDCODE THE OK
+                 */
+                Intent ok = new Intent(this, RegistrationokActivity.class);
+                startActivity(ok);
+                break;
+        }
     }
 
+  /*  private class UploadImage extends AsyncTask<Void, Void, Void> {
 
-    private void selectImage() {
+        Bitmap image;
 
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+        public  UploadImage(Bitmap image) {
+            this.image=image;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            image.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+            String encodedImage = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
+
+            ArrayList<NameValuePair> dataTosend = new ArrayList<>();
+            dataTosend.add(new BasicNameValuePair("image", encodedImage));
+
+
+            return null;
+        }
     }
+
+    private HttpParams getHttpRequestParams() {
+
+    } */
 }
