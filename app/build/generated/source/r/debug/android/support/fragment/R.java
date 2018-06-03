87974927fc92363d586a262f184ed602c5fc54ee
Giulia <giulia.pennati94@gmail.com>
2018-06-03 18:33:17 +0200

    new message form activity

--- app/build/generated/source/r/debug/android/support/fragment/R.java
+++ app/build/generated/source/r/debug/android/support/fragment/R.java
@@ -69,37 +69,37 @@ public final class R {
         public static final int action_image = 0x7f090011;
         public static final int action_text = 0x7f090017;
         public static final int actions = 0x7f090018;
-        public static final int async = 0x7f090021;
-        public static final int blocking = 0x7f090026;
-        public static final int chronometer = 0x7f090032;
-        public static final int forever = 0x7f090057;
-        public static final int icon = 0x7f09005f;
-        public static final int icon_group = 0x7f090060;
-        public static final int info = 0x7f090067;
-        public static final int italic = 0x7f090068;
-        public static final int line1 = 0x7f09006d;
-        public static final int line3 = 0x7f09006e;
-        public static final int normal = 0x7f090088;
-        public static final int notification_background = 0x7f090089;
-        public static final int notification_main_column = 0x7f09008a;
-        public static final int notification_main_column_container = 0x7f09008b;
-        public static final int right_icon = 0x7f09009d;
-        public static final int right_side = 0x7f09009e;
-        public static final int text = 0x7f0900cc;
-        public static final int text2 = 0x7f0900cd;
-        public static final int time = 0x7f0900d8;
-        public static final int title = 0x7f0900da;
+        public static final int async = 0x7f090022;
+        public static final int blocking = 0x7f090027;
+        public static final int chronometer = 0x7f090033;
+        public static final int forever = 0x7f090058;
+        public static final int icon = 0x7f090060;
+        public static final int icon_group = 0x7f090061;
+        public static final int info = 0x7f090068;
+        public static final int italic = 0x7f090069;
+        public static final int line1 = 0x7f09006e;
+        public static final int line3 = 0x7f09006f;
+        public static final int normal = 0x7f09008a;
+        public static final int notification_background = 0x7f09008b;
+        public static final int notification_main_column = 0x7f09008c;
+        public static final int notification_main_column_container = 0x7f09008d;
+        public static final int right_icon = 0x7f09009e;
+        public static final int right_side = 0x7f09009f;
+        public static final int text = 0x7f0900cf;
+        public static final int text2 = 0x7f0900d0;
+        public static final int time = 0x7f0900db;
+        public static final int title = 0x7f0900de;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f0a000a;
     }
     public static final class layout {
-        public static final int notification_action = 0x7f0b0036;
-        public static final int notification_action_tombstone = 0x7f0b0037;
-        public static final int notification_template_custom_big = 0x7f0b003e;
-        public static final int notification_template_icon_group = 0x7f0b003f;
-        public static final int notification_template_part_chronometer = 0x7f0b0043;
-        public static final int notification_template_part_time = 0x7f0b0044;
+        public static final int notification_action = 0x7f0b0037;
+        public static final int notification_action_tombstone = 0x7f0b0038;
+        public static final int notification_template_custom_big = 0x7f0b003f;
+        public static final int notification_template_icon_group = 0x7f0b0040;
+        public static final int notification_template_part_chronometer = 0x7f0b0044;
+        public static final int notification_template_part_time = 0x7f0b0045;
     }
     public static final class string {
         public static final int status_bar_notification_info_overflow = 0x7f0e0045;
---
+++ app/src/main/java/com/example/shadi/babycare/layout_view/MessageFormActivity.java
@@ -0,0 +1,25 @@
+package com.example.shadi.babycare.layout_view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.shadi.babycare.R;
+
+public class MessageFormActivity extends AppCompatActivity {
+
+    private TextView receiver;
+    private EditText message;
+    private Button send;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_form);
+
+        receiver = findViewById(R.id.receiver);
+        message = findViewById(R.id.formMessage);
+        send = findViewById(R.id.send_message);
+    }
+}
