978cab0f195917d5858a180d05740cff88785cf7
Giulia <giulia.pennati94@gmail.com>
2018-06-04 20:18:15 +0200

    policy activity

---
+++ app/src/main/java/com/example/shadi/babycare/layout_view/PolicyActivity.java
@@ -0,0 +1,18 @@
+package com.example.shadi.babycare.layout_view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.shadi.babycare.R;
+
+public class PolicyActivity extends AppCompatActivity {
+
+    private TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_policy);
+        text = findViewById(R.id.policy);
+    }
+}
