0b587f380f6eced98b754a91e00ba5c1bbf7508d
Giulia <giulia.pennati94@gmail.com>
2018-06-14 20:54:16 +0200

    updated details

--- app/src/main/java/com/example/shadi/babycare/layout_view/ReviewActivity.java
+++ app/src/main/java/com/example/shadi/babycare/layout_view/ReviewActivity.java
@@ -4,10 +4,15 @@ import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.FrameLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.example.shadi.babycare.R;
 
 public class ReviewActivity extends BaseActivity {
+
+    private TextView bs, rw, title, text;
+    private RatingBar rating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,12 @@ public class ReviewActivity extends BaseActivity {
 
         super.setTitle("Review");
 
-;
+        bs = findViewById(R.id.review_detail_bs_name);
+        rw = findViewById(R.id.rev_detail_pr_name);
+        title = findViewById(R.id.review_detail_title);
+        text = findViewById(R.id.review_detail_txt);
+        rating = findViewById(R.id.review_detail_rating);
+
+
     }
 }
